cf59e25ac3b75ee0d5ac8277f292d948472c8d47
Unknown <doudoupike@hotmail.fr>
2017-11-19 18:59:17 +0100

    update template

--- src/UserBundle/Controller/SecurityController.php
+++ src/UserBundle/Controller/SecurityController.php
@@ -5,7 +5,7 @@ namespace UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class UserController extends Controller
+class SecurityController extends Controller
 {
     public function indexAction()
     {
