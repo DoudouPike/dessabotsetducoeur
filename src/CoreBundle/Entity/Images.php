e6b2a664140831d133d26b8987f9c71aed74fa20
Unknown <doudoupike@hotmail.fr>
2018-02-09 00:38:07 +0100

    init file image creation

--- src/CoreBundle/Entity/Images.php
+++ src/CoreBundle/Entity/Images.php
@@ -30,11 +30,15 @@ class Images
     /**
      * @var string
      *
-     * @Vich\UploadableField(mapping="images", fileNameProperty="image")
-     *
      * @ORM\Column(name="url", type="string", length=255)
      */
-    private $url;
+    private $image;
+
+    /**
+     * @var File
+     * @Vich\UploadableField(mapping="advert_images", fileNameProperty="image")
+     */
+    private $file;
 
     /**
      * @var string
@@ -46,7 +50,7 @@ class Images
     /**
      * @var \DateTime
      *
-     * @ORM\Column(name="$update_at", type="datetime")
+     * @ORM\Column(name="update_at", type="datetime")
      */
     private $updateAt;
 
@@ -69,33 +73,44 @@ class Images
     }
 
     /**
-     * Set url
+     * @param File $file
+     */
+    public function setFile(File $image=null)
+    {
+        $this->file = $image;
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updateAt = new \DateTime('now');
+        }
+    }
+
+    /**
+     * @return File
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Set image
      *
-     * @param string $url
+     * @param string $image
      *
      * @return Images
      */
-    public function setUrl(File $url = null)
+    public function setImage($image)
     {
-        $this->url = $url;
-
-        // VERY IMPORTANT:
-        // It is required that at least one field changes if you are using Doctrine,
-        // otherwise the event listeners won't be called and the file is lost
-        if ($image) {
-            // if 'updatedAt' is not defined in your entity, use another property
-            $this->updatedAt = new \DateTime('now');
-        }
-
+        $this->image = $image;
         return $this;
     }
 
     /**
-     * Get url
+     * Get image
      *
      * @return string
      */
-    public function getUrl()
+    public function getImage()
     {
         return $this->url;
     }
